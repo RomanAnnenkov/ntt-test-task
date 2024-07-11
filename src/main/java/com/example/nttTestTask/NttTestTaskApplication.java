19231f0ce5672bfa30f93846c1c82fc7274eb767
Roman Annenkov <roman.annenkov@gmail.com>
2024-07-11 12:31:53 +0300

    empty project added

---
+++ src/main/java/com/example/nttTestTask/NttTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.nttTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttTestTaskApplication.class, args);
+	}
+
+}
